6c04c5de7c785ca2a86bff825625d129e276072c
Marcin Cudny <bekinekt@gmail.com>
2022-06-24 04:39:29 +0200

    adding stuff
    does not work +poczatek. fix

--- ProjectStudy/main.c
+++ ProjectStudy/main.c
@@ -3,127 +3,143 @@
 #include <string.h>
 
 struct osobaStr {
-    int nrRejstr;
-    struct osobaStr* nextInLine;
-    char imieWlasciciela[64];
+	int nrRejstr;
+	struct osobaStr* next;
 };
 
 struct osobaStr* dodajRejstracje(const int age)
 {
-    struct osobaStr* nowaOsoba = NULL;
-    nowaOsoba = malloc(sizeof(struct osobaStr));
-    if (nowaOsoba != NULL)
-    {
-        nowaOsoba->nextInLine = NULL;
-        nowaOsoba->nrRejstr = age;
-        printf("stworzono nowa rejstracje w pamieci: %p\n", nowaOsoba);
-    }
-    else
-    {
-        printf("Error: nie udalo sie dodac rejstracji \n");
-    }
-    return nowaOsoba;
+	struct osobaStr* nowaOsoba = NULL;
+	nowaOsoba = malloc(sizeof(struct osobaStr));
+	if (nowaOsoba != NULL)
+	{
+		nowaOsoba->next = NULL;
+		nowaOsoba->nrRejstr = age;
+		printf("stworzono nowa rejstracje w pamieci: %p\n", nowaOsoba);
+	}
+	else
+	{
+		printf("Error: nie udalo sie dodac rejstracji \n");
+	}
+	return nowaOsoba;
+}
+
+void push(struct osobaStr** dodajRejstracje, int new_data)
+{
+	struct osobaStr* replaceOne = (struct osobaStr*)malloc(sizeof(struct osobaStr));
+	replaceOne->nrRejstr = new_data;
+	replaceOne->next = (*dodajRejstracje);
+	(*dodajRejstracje) = replaceOne;
 }
 
 void printPerson(const struct osobaStr* person, const char* comment)
 {
-    if (person == NULL)
-    {
-        printf("%s == NULL\n", comment);
-    }
-    else
-    {
-        printf("%s: Rejstracja:%d address:%p nextInLine:%p\n",
-            comment,
-            person->nrRejstr,
-            person,
-            person->nextInLine);
-    }
+	if (person == NULL)
+	{
+		printf("%s == NULL\n", comment);
+	}
+	else
+	{
+		printf("%s: Rejstracja:%d address:%p nextInLine:%p\n",
+			comment,
+			person->nrRejstr,
+			person,
+			person->next);
+	}
 
 }
 
 void wydrukujListe(const struct osobaStr* list)
 {
-    printf("Drukuje liste:\n");
-    const struct osobaStr* t;
-    t = list;
-    if (t == NULL)
-    {
-        printf("lista jest pusta\n");
-    }
-    else
-    {
-        while (t)
-        {
-            printPerson(t, "t");
-            t = t->nextInLine;
-        }
-    }
+	printf("Drukuje liste:\n");
+	const struct osobaStr* t;
+	t = list;
+	if (t == NULL)
+	{
+		printf("lista jest pusta\n");
+	}
+	else
+	{
+		while (t)
+		{
+			printPerson(t, "t");
+			t = t->next;
+		}
+	}
 }
 
 void czyszczenie(struct osobaStr* list)
 {
-    struct osobaStr* next;
-    while (list)
-    {
-        next = list->nextInLine;
-        printf("Czyszcze %d\n", list->nrRejstr);
-        free(list);
-        list = next;
-    }
+	struct osobaStr* next;
+	while (list)
+	{
+		next = list->next;
+		printf("Czyszcze %d\n", list->nrRejstr);
+		free(list);
+		list = next;
+	}
 }
 
 int main()
 {
-    printf("Lista komend:\n x - zamknij program,\n wydrukuj - wydrukuj dane w liscie,\n [liczba] - dodaje rejstracje z podana liczba\n");
+	printf("Lista komend:\n x - zamknij program,\n wydrukuj - wydrukuj dane w liscie,\n [liczba] - dodaje rejstracje z podana liczba\n");
 
-    struct osobaStr* one = NULL;
-    struct osobaStr* nextOne = NULL;
+	struct osobaStr* one = NULL;
+	struct osobaStr* nextOne = NULL;
 
-    char komenda[64];
-    int age;
+	char komenda[64];
+	int age;
 
-    while (1)
-    {
-        printf("Enter a command or value : ");
-        fgets(komenda, 64, stdin);
-        if (strcmp("x\n", komenda) == 0)
-        {
-            printf("Zamykam program..\n");
-            break;
-        }
-        else if (strcmp("wydrukuj\n", komenda) == 0)
-        {
-            printf("Drukuje..\n");
-            wydrukujListe(one);
-        }
-        else if (sscanf(komenda, "%d", &age) != NULL)
-        {
-            printf("Dodaje %d\n", age);
-            if (one == NULL)
-            {
-                one = dodajRejstracje(age);
-                if (one != NULL)
-                {
-                    nextOne = one;
-                }
-            }
-            else
-            {
-                nextOne->nextInLine = dodajRejstracje(age);
-                if (nextOne->nextInLine != NULL)
-                {
-                    nextOne = nextOne->nextInLine;
-                }
-            }
+	while (1)
+	{
+		printf("Wpisz polecenie: ");
 
-        }
-    }
+		fgets(komenda, 64, stdin);
+		if (strcmp("x\n", komenda) == 0)
+		{
+			printf("Zamykam program..\n");
+			break;
+		}
 
-    czyszczenie(one);
-    one = NULL;
-    nextOne = NULL;
+		else if (strcmp("wydrukuj\n", komenda) == 0)
+		{
+			printf("Drukuje..\n");
+			wydrukujListe(one);
+		}
 
-    return 0;
+		else if (strcmp("+poczatek\n", komenda) == 0)
+		{
+			sscanf(komenda, "%d", &age);
+			printf("Podaj rejstracje zeby dodac ja na poczatek listy:\n");
+			push(&one, age);
+		}
+
+		else if (sscanf(komenda, "%d", &age) != NULL)
+		{
+			printf("Dodaje %d\n", age);
+			if (one == NULL)
+			{
+				one = dodajRejstracje(age);
+				if (one != NULL)
+				{
+					nextOne = one;
+				}
+			}
+			else
+			{
+				nextOne->next = dodajRejstracje(age);
+				if (nextOne->next != NULL)
+				{
+					nextOne = nextOne->next;
+				}
+			}
+
+		}
+	}
+
+	czyszczenie(one);
+	one = NULL;
+	nextOne = NULL;
+
+	return 0;
 }
-
